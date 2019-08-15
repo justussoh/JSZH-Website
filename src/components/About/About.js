965d788d15dba65ca15bb3ea7b2388535ef47fb5
Justus Soh <justus.soh@hotmail.com>
2019-08-16 00:53:44 +0800

    About Implemented

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,15 +13,18 @@ class About extends React.Component {
 
         return (
             <Styles>
-                <Container fluid style={{height:"100vh"}}>
-                    <h3>About</h3>
+                <Container fluid style={{height:"80vh", padding: '30px 0px' }}>
+                    <Col xs={{span:10, offset:1}}>
                     <Row>
-                        <Col>
-                            <img src=''/>
+                        <Col xs={false} md={6}>
+                            <img src='/images/about.jpg' alt='about' style={{width:'100%'}}/>
                         </Col>
-                        <Col>
+                        <Col xs={12} md={6} className='d-flex align-items-start justify-content-center flex-column'>
+                            <h2>About Me</h2>
+                            <p>I am currently a Year 2 Business Analytics Student in the National University of Singapore, School of Computing. I have a keen interest in programming and statistics. I am looking for an internship position at a organization to utilize the education qualifications in Business Analytics and Computing.</p>
                         </Col>
                     </Row>
+                    </Col>
                 </Container>
             </Styles>
         );
