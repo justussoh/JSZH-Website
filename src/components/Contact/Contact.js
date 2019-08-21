15cf9a6f1591961ffb6762eb6a01269b401e2635
Justus Soh <justus.soh@hotmail.com>
2019-08-21 16:43:05 +0800

    Map added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,8 +2,10 @@ import React from 'react';
 import history from '../../history';
 import styled from 'styled-components';
 import {Container, Col, Row} from 'react-bootstrap';
+import ReactMapGL from 'react-map-gl';
 
 import './Contact.css'
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 const Styles = styled.div`
     .font-color{
@@ -14,7 +16,13 @@ const Styles = styled.div`
         color:#08fdd8;
         font-weight: bold;
     }
+    
+    .mapboxgl-ctrl-logo{
+        display: none !important;
+    }
 `;
+
+const MAPBOX_TOKEN = "pk.eyJ1IjoianVzdHVzc29oIiwiYSI6ImNqemt4cmwwcjBpN2kzYnNhMTNjMWptd3oifQ.My8I-eSghEV14L3CCu6vDA";
 
 class Contact extends React.Component {
 
@@ -24,6 +32,13 @@ class Contact extends React.Component {
         subject: '',
         message: '',
         focus: false,
+        viewport: {
+            width: '100%',
+            height: '100%',
+            latitude: 1.351616,
+            longitude: 103.808053,
+            zoom: 10
+        }
     };
 
     handleName = (e) => {
@@ -48,20 +63,20 @@ class Contact extends React.Component {
 
     render() {
         return (
-            <Container fluid style={{height: '100vh', backgroundColor: "#252627"}}
+            <Container fluid style={{height: '100vh', backgroundColor: "#252627",padding:0, overflow:'hidden'}}
                        className='d-flex justify-content-center align-items-center'>
-                <Styles style={{width: '100%'}}>
-                    <Col xs={{span: 10, offset: 1}}>
-                        <Row>
-                            <Col xs={12} md={6} className='d-flex align-items-start justify-content-center flex-column'>
-                                <Col>
-                                    <h2 className='font-title'>Contact Me</h2>
-                                    <p className='font-color'>If you have any questions, don't hesitate to contact me
-                                        with the form below</p>
-                                </Col>
-                                <div>
-                                    <Row>
-                                        <Col>
+                <Styles style={{width: '100%',height:'100%'}}>
+                    <Row style={{height:'100%'}}>
+                        <Col xs={12} md={{span:4, offset:1}} className='d-flex align-items-start justify-content-center flex-column'>
+
+                            <div style={{padding:15}}>
+                                <h2 className='font-title'>Contact Me</h2>
+                                <p className='font-color'>If you have any questions, don't hesitate to contact me
+                                    with the form below</p>
+                            </div>
+                            <div>
+                                <Row>
+                                    <Col>
                                     <span className="input input--nao">
                                         <input className="input__field input__field--nao" type="text" id="input-1"
                                                onChange={this.handleName}/>
@@ -77,10 +92,11 @@ class Contact extends React.Component {
                                                 d="M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0"/>
                                         </svg>
 				                    </span>
-                                        </Col>
-                                        <Col>
+                                    </Col>
+                                    <Col>
                                         <span className="input input--nao">
-                                        <input className="input__field input__field--nao" type="text" id="input-4" onChange={this.handleEmail}/>
+                                        <input className="input__field input__field--nao" type="text" id="input-4"
+                                               onChange={this.handleEmail}/>
                                         <label
                                             className={this.state.email !== '' ? "input__label input__label--nao input__label--nao--filled" : "input__label input__label--nao"}
                                             htmlFor="input-4">
@@ -94,12 +110,13 @@ class Contact extends React.Component {
                                                 d="M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0"/>
                                         </svg>
 				                    </span>
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
                                     <span className="input input--nao">
-                                        <input className="input__field input__field--nao" type="text" id="input-2" onChange={this.handleSubject}/>
+                                        <input className="input__field input__field--nao" type="text" id="input-2"
+                                               onChange={this.handleSubject}/>
                                         <label
                                             className={this.state.subject !== '' ? "input__label input__label--nao input__label--nao--filled" : "input__label input__label--nao"}
                                             htmlFor="input-2">
@@ -113,10 +130,10 @@ class Contact extends React.Component {
                                                 d="M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0"/>
                                         </svg>
                                     </span>
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
                                     <span className="input input--nao">
                                         <textarea rows="4" className="input__field input__field--nao" id="input-3"
                                                   onChange={this.handleMessage} onFocus={this.handleFocus}
@@ -134,15 +151,19 @@ class Contact extends React.Component {
                                                 d="M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0"/>
                                         </svg>
                                     </span>
-                                        </Col>
-                                    </Row>
-                                </div>
-                            </Col>
-                            <Col xs={false} md={6}>
-
-                            </Col>
-                        </Row>
-                    </Col>
+                                    </Col>
+                                </Row>
+                            </div>
+                        </Col>
+                        <Col xs={false} md={{span:6, offset:1}}>
+                            <ReactMapGL
+                                {...this.state.viewport}
+                                onViewportChange={(viewport) => this.setState({viewport})}
+                                mapboxApiAccessToken={MAPBOX_TOKEN}
+                                mapStyle={'mapbox://styles/justussoh/cjzkz0evc02sv1cozec2q5cjs'}
+                            />
+                        </Col>
+                    </Row>
                 </Styles>
             </Container>
         );
