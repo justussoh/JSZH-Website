5847bca8229b9dd4d1b387431eccf1a38356c7c3
Justus Soh <justus.soh@hotmail.com>
2019-10-09 18:15:32 +0800

    Skills Updated

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -9,29 +9,52 @@ import {CSSTransition} from "react-transition-group";
 import MediaQuery from "react-responsive";
 import Typist from 'react-typist';
 import 'react-typist/dist/Typist.css';
+import WOW from "wow.js";
 
-const data = [
+const dataAnalytics = [
     [
         'Skill',
         'Level',
         {role: 'style'},
     ],
-    ['R', 76, '#fcbd0b'],
-    ['Python', 80, '#fcbd0b'],
-    ['Making Tea', 99, '#fcbd0b'],
-    ['JavaScipt', 70, '#fcbd0b'],
-    ['Tableau', 77, '#fcbd0b'],
+    ['Data Visualisation', 85, '#fcbd0b'],
+    ['Predictive Modeling', 80, '#fdca3c'],
+    ['Data Manipulation', 77, '#fdd154'],
+    ['Computer Vision', 60, '#fdd76d'],
+    ['Optimization', 55, '#fee59d'],
+];
+
+const dataProg = [
+    [
+        'Skill',
+        'Level',
+        {role: 'style'},
+    ],
+    ['JavaScipt', 90, '#fcbd0b'],
+    ['Java', 85, '#fdca3c'],
+    ['HTML/CSS/JSX', 84, '#fdd154'],
+    ['Python', 80, '#fdd76d'],
+    ['R', 76, '#fee59d'],
 ];
 
 const Styles = styled.div`
     .font{
         color:white;
+       
     }
     
     .font-title{
         color:#fcbd0b;
         font-weight: bold;
         font-size:28px;
+        text-align:center;
+    }
+    
+    .font-subtitle{
+        color:#fcbd0b;
+        font-weight: bold;
+        font-size:14px;
+        text-align:center;
     }
     
     .font-link{
@@ -49,7 +72,9 @@ class Skills extends React.Component {
     };
 
     componentDidMount() {
-        window.setTimeout(() => this.setState({isLoading: false}), 1000)
+        window.setTimeout(() => this.setState({isLoading: false}), 1000);
+        const wow = new WOW();
+        wow.init();
     }
 
     handleClick = () => {
@@ -65,72 +90,116 @@ class Skills extends React.Component {
                 return (
                     <Container fluid style={{height: '100vh'}}
                                className='d-flex justify-content-center align-items-center'>
-                        <Styles>
-                            <Col xs={{span: 10, offset: 1}}>
+                        <Styles style={{width: '70vw'}}>
+                            <Col xs={{span: 11, offset: 0}}>
                                 <Row>
-                                    <Col xs={12} md={5}>
+                                    <Col xs={12} md={3} className='d-flex justify-content-center align-items-center' >
                                         <Typist className='font-title' startDelay={1250}>
                                             WHAT I CAN DO
                                         </Typist>
-                                        <p className='font'>
-                                            The main area of my expertise is in Data Analytics (Descriptive,
-                                            Prescriptive and Predictive Analytics) using R and Python. In addition, I
-                                            can also do Data Visualization using Tableau to transform data into a more
-                                            readable format to gain better insight.
-                                        </p>
-                                        <p className='font'>I am also skilled in Data Management on relational databases
-                                            like mySQL and non-relational like MongoDB.</p>
-                                        <p className='font'>I can also do full stack web development (HTML, CSS and
-                                            JavaScript), familiar with MERN stack.</p>
-                                        <p className='font'> I also have knowledge in using Illustrator, Photoshop,
-                                            InDesign and XD for design. And am fluent in spoken and written English and
-                                            Chinese.</p>
-                                        <p className='font'>Visit my <a href='https://www.linkedin.com/in/justussoh/'
-                                                                        target='_blank'
-                                                                        className='font-link'>LinkedIn</a>, <a
-                                            href='https://github.com/justussoh' target='_blank'
-                                            className='font-link'>Github</a> or <a
-                                            href='https://public.tableau.com/profile/justus.soh#!/'
-                                            target='_blank' className='font-link'>Tableau</a> profiles for more details,
-                                            or
-                                            just <span
-                                                onClick={this.handleClick} className='font-link'>contact</span> me! </p>
                                     </Col>
-                                    <MediaQuery minWidth={426}>
-                                        <Col xs={false} md={7}>
-                                            <Chart
-                                                chartType="ColumnChart"
-                                                width="100%"
-                                                height="100%"
-                                                data={data}
-                                                options={{
-                                                    legend: 'none',
-                                                    animation: {
-                                                        startup: true,
-                                                        easing: 'linear',
-                                                        duration: 1500,
-                                                    },
-                                                    backgroundColor: 'none',
-                                                    hAxis: {
-                                                        baselineColor: '#fff',
-                                                        gridlines: {
-                                                            count: 0
+                                    <Col xs={12} md={9}>
+                                        <Row>
+                                            <Col xs={12} md={3}
+                                                 className='d-flex justify-content-center align-items-center'>
+                                                <h6 className='font wow fadeIn' data-wow-delay='1s'
+                                                    data-wow-offset='20'>
+                                                    PROGRAMMING SKILLS
+                                                </h6>
+                                            </Col>
+                                            <Col xs={12} md={9}>
+                                                <Chart
+                                                    chartType="BarChart"
+                                                    width="100%"
+                                                    height="40vh"
+                                                    data={dataProg}
+                                                    options={{
+                                                        legend: 'none',
+                                                        animation: {
+                                                            startup: true,
+                                                            easing: 'linear',
+                                                            duration: 1250,
                                                         },
-                                                        textStyle: {
-                                                            color: '#fff'
+                                                        backgroundColor: 'none',
+                                                        hAxis: {
+                                                            minValue: 0,
+                                                            maxValue: 100,
+                                                            baselineColor: '#fff',
+                                                            gridlines: {
+                                                                count: 0
+                                                            },
+                                                            textStyle: {
+                                                                color: '#fff'
+                                                            },
                                                         },
-                                                    },
-                                                    vAxis: {
-                                                        minValue: 0,
-                                                        textStyle: {
-                                                            color: '#fff'
+                                                        vAxis: {
+                                                            minValue: 0,
+                                                            textStyle: {
+                                                                color: '#fff'
+                                                            },
+                                                            baselineColor: '#fff'
                                                         },
-                                                        baselineColor: '#fff'
-                                                    },
-                                                }}
-                                            />
-                                        </Col>
-                                    </MediaQuery>
+                                                    }}
+                                                />
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col xs={12} sm={3}
+                                                 className='d-flex justify-content-center align-items-center'>
+                                                <h6 className='font wow fadeIn' data-wow-delay='1s'
+                                                    data-wow-offset='20'>
+                                                    DATA ANALYTICS SKILLS
+                                                </h6>
+                                            </Col>
+                                            <Col xs={12} sm={9}>
+                                                <Chart
+                                                    chartType="BarChart"
+                                                    width="100%"
+                                                    height="35vh"
+                                                    data={dataAnalytics}
+                                                    options={{
+                                                        legend: 'none',
+                                                        animation: {
+                                                            startup: true,
+                                                            easing: 'linear',
+                                                            duration: 1250,
+                                                        },
+                                                        backgroundColor: 'none',
+                                                        hAxis: {
+                                                            minValue: 0,
+                                                            maxValue: 100,
+                                                            baselineColor: '#fff',
+                                                            gridlines: {
+                                                                count: 0
+                                                            },
+                                                            textStyle: {
+                                                                color: '#fff'
+                                                            },
+                                                        },
+                                                        vAxis: {
+                                                            minValue: 0,
+                                                            textStyle: {
+                                                                color: '#fff'
+                                                            },
+                                                            baselineColor: '#fff'
+                                                        },
+                                                    }}
+                                                />
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                </Row>
+                                <Row className='d-flex justify-content-center align-items-center'>
+                                    <p className='font'>Visit my <a href='https://www.linkedin.com/in/justussoh/'
+                                                                    target='_blank'
+                                                                    className='font-link'>LinkedIn</a>, <a
+                                        href='https://github.com/justussoh' target='_blank'
+                                        className='font-link'>Github</a> or <a
+                                        href='https://public.tableau.com/profile/justus.soh#!/'
+                                        target='_blank' className='font-link'>Tableau</a> profiles for more details,
+                                        or
+                                        just <span
+                                            onClick={this.handleClick} className='font-link'>contact</span> me! </p>
                                 </Row>
                             </Col>
                         </Styles>
